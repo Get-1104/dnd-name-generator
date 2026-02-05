7ac0d8a97b99d74fbfc2c475c0030a2e562348f8
fulin <751932804@qq.co>
2026-02-05 13:43:12 +0800

    chore: remove Next.js configuration file

---
+++ lib/namePools/human/western.ts
@@ -0,0 +1,84 @@
+"use client";
+
+export const WESTERN = {
+  given: {
+    masculine: [
+      // Classic medieval-fantasy (5-7 chars)
+      "Alden", "Alric", "Beren", "Cedric", "Corwin",
+      "Daren", "Edric", "Eldon", "Gareth", "Harlan",
+      "Jonas", "Kellen", "Leoric", "Marten", "Orrin",
+      "Perrin", "Rowan", "Soren", "Tarin", "Ulric",
+      "Wesley", "Calder", "Bram", "Holden", "Oswin",
+      // Short & punchy (3-5 chars)
+      "Brant", "Cole", "Drake", "Erik", "Finn",
+      "Grant", "Hugh", "Jace", "Kent", "Lance",
+      "Miles", "Nolan", "Pierce", "Reed", "Seth",
+      "Wade", "Zane", "Boyd", "Dale", "Ward",
+      // Longer/formal (8+ chars)
+      "Aldrich", "Benedict", "Caspian", "Dominic", "Edmund",
+      "Frederick", "Geoffrey", "Hadrian", "Ignatius", "Jasper",
+      "Kendrick", "Lawrence", "Matthias", "Nicholas", "Percival",
+      "Quentin", "Roderick", "Sebastian", "Theodore", "Warwick",
+    ],
+    feminine: [
+      // Classic medieval-fantasy (5-7 chars)
+      "Adela", "Brina", "Celia", "Daphne", "Elara",
+      "Fiona", "Gwenna", "Helena", "Isolde", "Jessa",
+      "Kara", "Liora", "Maren", "Nessa", "Odette",
+      "Petra", "Roslyn", "Selene", "Talia", "Vera",
+      "Willa", "Clara", "Elowen", "Maris", "Lydia",
+      // Short & punchy (3-5 chars)
+      "Ada", "Bess", "Clare", "Dawn", "Elle",
+      "Faith", "Grace", "Hope", "Iris", "June",
+      "Kate", "Lena", "Mae", "Nina", "Rose",
+      "Sara", "Vera", "Wren", "Zara", "Blair",
+      // Longer/formal (8+ chars)
+      "Adelaide", "Beatrice", "Catherine", "Delphine", "Eleanor",
+      "Francesca", "Genevieve", "Isabelle", "Josephine", "Katherine",
+      "Magdalene", "Nicolette", "Rosalind", "Seraphina", "Vivienne",
+      "Marguerite", "Gwendolyn", "Evangeline", "Celestine", "Arabella",
+    ],
+    neutral: [
+      "Arden", "Avery", "Bailey", "Briar", "Cameron",
+      "Casey", "Devon", "Ellis", "Emery", "Finley",
+      "Harper", "Hayden", "Jordan", "Kai", "Logan",
+      "Morgan", "Parker", "Quinn", "Reese", "Riley",
+      "Rowan", "Sage", "Taylor", "Winter", "Blake",
+    ]
+  },
+  surnames: {
+    occupational: [
+      "Archer", "Baker", "Brewer", "Carpenter", "Carter",
+      "Chandler", "Clark", "Cooper", "Fletcher", "Fuller",
+      "Gardener", "Harper", "Hunter", "Mason", "Mercer",
+      "Miller", "Porter", "Sawyer", "Shepherd", "Smith",
+      "Tanner", "Taylor", "Thatcher", "Turner", "Wainwright",
+      "Walker", "Weaver", "Wheeler", "Wright", "Fisher",
+      "Fowler", "Glover", "Hayward", "Saddler", "Cutler",
+    ],
+    patronymic: [
+      "Alderson", "Aldric", "Branson", "Coleson", "Edricson",
+      "Ericson", "Garethson", "Harlanson", "Jonason", "Kellenson",
+      "Martenson", "Orrinson", "Perrinson", "Rowanson", "Sorenson",
+      "Wesleyson", "Calderson", "Bramson", "Holdenson", "Corwinson",
+      "Aldwin", "Godwin", "Goodwin", "Baldwin", "Darwin",
+      "Fitzroy", "Fitzwilliam", "MacLeod", "O'Brien", "Donovan",
+    ],
+    toponymic: [
+      "Ashford", "Blackwood", "Brookfield", "Clayborne", "Coldwell",
+      "Dunford", "Eastmere", "Fairfield", "Glenwood", "Greenfield",
+      "Greystone", "Hillbrook", "Ironbridge", "Kingswell", "Lakewood",
+      "Lightford", "Meadowell", "Moorfield", "Northwell", "Oakridge",
+      "Ravenwood", "Redbrook", "Riverwell", "Stonefield", "Thornbury",
+      "Westbrook", "Whitford", "Willowdale", "Woodhaven", "Ashwood",
+      "Brightwell", "Clearwater", "Deepwood", "Elmwood", "Fallbrook",
+    ],
+    ornamental: [
+      "Blackthorn", "Brightsword", "Clearwater", "Deepwood", "Fairwind",
+      "Goldheart", "Greymane", "Hawthorne", "Ironheart", "Lionheart",
+      "Ravenwood", "Silverthorn", "Starling", "Stormrider", "Swiftblade",
+      "Thornfield", "Trueheart", "Whitestone", "Wildrose", "Windrider",
+      "Brightstar", "Fairborne", "Goldleaf", "Nightingale", "Silvermoon",
+    ]
+  }
+} as const;
