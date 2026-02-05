dbac81b04f4981be1665a09adc6f4c2ecd11a13e
fulin <751932804@qq.co>
2026-02-05 13:41:55 +0800

    chore: delete unused Next.js configuration file

---
+++ lib/namePools/human/eastern.ts
@@ -0,0 +1,77 @@
+"use client";
+
+export const EASTERN = {
+  given: {
+    masculine: [
+      // Classical/Byzantine inspired (6-8 chars)
+      "Alaric", "Cassian", "Darian", "Evander", "Hadrian",
+      "Iskander", "Julius", "Lucian", "Marcell", "Nerion",
+      "Octavian", "Orest", "Pavlen", "Roman", "Severin",
+      "Tiber", "Valerian", "Xander", "Zorian", "Leontius",
+      "Basil", "Constantin", "Dimitri", "Ilias", "Maximus",
+      // Short elegant (4-6 chars)
+      "Arian", "Cyrus", "Darius", "Emil", "Gregor",
+      "Kael", "Leo", "Marius", "Nikos", "Orion",
+      "Titus", "Valen", "Zeno", "Alexei", "Boris",
+      // Longer/formal (9+ chars)
+      "Alexander", "Antonius", "Bartholomew", "Cornelius", "Demetrius",
+      "Eustace", "Gregorius", "Honorius", "Justinian", "Kyrillos",
+      "Leonidas", "Maxentius", "Nicodemus", "Theodosius", "Valentinus",
+      "Alexandros", "Christoforos", "Konstantinos", "Sebastianos", "Theodoros",
+    ],
+    feminine: [
+      // Classical/Byzantine inspired (6-8 chars)
+      "Ariana", "Cassia", "Daria", "Eliana", "Flavia",
+      "Helena", "Ilona", "Jasmina", "Larissa", "Milena",
+      "Nadia", "Octavia", "Selina", "Tatiana", "Valeria",
+      "Zara", "Marina", "Evelina", "Sabina", "Katerina",
+      "Livia", "Noemi", "Serena", "Yelena", "Isadora",
+      // Short elegant (4-6 chars)
+      "Aella", "Cleo", "Dora", "Elena", "Irene",
+      "Julia", "Lyra", "Maia", "Nora", "Phoebe",
+      "Rhea", "Sofia", "Thea", "Vera", "Zara",
+      // Longer/formal (9+ chars)
+      "Alessandra", "Anastasia", "Aurelia", "Cassandra", "Cleopatra",
+      "Euphemia", "Theodora", "Valentina", "Yekaterina", "Evangeline",
+      "Ekaterina", "Elisabeta", "Margareta", "Seraphina", "Victoriana",
+    ],
+    neutral: [
+      "Alexis", "Ari", "Aurel", "Cyril", "Dorian",
+      "Emil", "Ira", "Jules", "Kyran", "Luca",
+      "Mika", "Niko", "Orion", "Remy", "Sasha",
+      "Tariq", "Vale", "Zen", "Noor", "Silas",
+    ]
+  },
+  surnames: {
+    occupational: [
+      "Alchemist", "Architect", "Armorer", "Cartwright", "Clockmaker",
+      "Goldsmith", "Jeweler", "Mapmaker", "Merchant", "Scribe",
+      "Silversmith", "Steward", "Tinker", "Vintner", "Wheelwright",
+      "Artificer", "Bookbinder", "Chandler", "Harper", "Inkmaker",
+      "Locksmith", "Perfumer", "Potter", "Scholar", "Silkweaver",
+      "Spicemerchant", "Tilemaker", "Weaver", "Wordsmith", "Ceramist",
+    ],
+    patronymic: [
+      "Alexandrovich", "Antonovich", "Basilovich", "Constantinovich", "Demetriovich",
+      "Gregorovich", "Ivanovich", "Mikhailovich", "Nikolaievich", "Pavlovich",
+      "Romanovich", "Sergeiovich", "Vasilievich", "Vladimirovich", "Yurievich",
+      "Adrianides", "Chrysanthos", "Dimitrios", "Georgios", "Konstantinos",
+      "Marcellius", "Theodosian", "Valentius", "Xenophon", "Zacharias",
+    ],
+    toponymic: [
+      "Argentian", "Aurelios", "Byzantos", "Chrysolis", "Coronel",
+      "Delphos", "Ephesian", "Helios", "Imperialis", "Korinthios",
+      "Lysandros", "Magnolia", "Novoros", "Palantir", "Rhodian",
+      "Selenar", "Solarium", "Thessalon", "Valyros", "Zarephath",
+      "Adrianople", "Antiochus", "Caesarea", "Damascene", "Phoenicia",
+      "Alexandrite", "Cordovan", "Damascene", "Florentine", "Venetian",
+    ],
+    ornamental: [
+      "Angelfire", "Brightstar", "Crystalborn", "Dawnbringer", "Goldencrown",
+      "Ironheart", "Lightbringer", "Moonwhisper", "Nightstar", "Phoenixborn",
+      "Sapphire", "Silverstar", "Starheart", "Sunblade", "Twilightborn",
+      "Azurewind", "Ebonstar", "Goldenwing", "Ivoryshield", "Morningstar",
+      "Obsidianshield", "Pearlheart", "Rubycrystal", "Starweaver", "Topazborn",
+    ]
+  }
+} as const;
---
+++ lib/namePools/human/northern.ts
@@ -0,0 +1,78 @@
+"use client";
+
+export const NORTHERN = {
+  given: {
+    masculine: [
+      // Norse-inspired (5-7 chars)
+      "Bjorn", "Dag", "Eirik", "Fenric", "Gunnar",
+      "Hakon", "Halvor", "Ivar", "Jorund", "Kjell",
+      "Leif", "Magnar", "Njal", "Oskar", "Ragnar",
+      "Rurik", "Sigurd", "Sven", "Thorin", "Ulfric",
+      "Viggo", "Wulfrun", "Yorik", "Arvid", "Sten",
+      // Short & harsh (3-5 chars)
+      "Arn", "Bors", "Egil", "Falk", "Grim",
+      "Hak", "Jarl", "Kol", "Lars", "Olf",
+      "Rolf", "Tor", "Ulf", "Vidar", "Wulf",
+      // Longer/formal (8+ chars)
+      "Alaric", "Baldric", "Branimir", "Caradoc", "Dietrich",
+      "Eldric", "Grimbald", "Hjalmar", "Ingomar", "Jorvik",
+      "Kolgrim", "Ludovic", "Madrik", "Norgrim", "Ragnarok",
+      "Sigmund", "Thorgrim", "Ulfgrim", "Vladimir", "Wolfric",
+    ],
+    feminine: [
+      // Norse-inspired (5-7 chars)
+      "Astrid", "Brynja", "Dagny", "Eira", "Freya",
+      "Gudrun", "Hilda", "Inga", "Kari", "Liv",
+      "Mira", "Nora", "Ragna", "Sigrid", "Solveig",
+      "Thyra", "Una", "Vala", "Yrsa", "Runa",
+      "Helga", "Sanna", "Rikke", "Tove", "Eydis",
+      // Short & strong (3-5 chars)
+      "Asta", "Bera", "Frida", "Greta", "Hella",
+      "Idun", "Saga", "Thora", "Vara", "Ylva",
+      // Longer/formal (8+ chars)
+      "Brunhilde", "Gunnhild", "Ingeborg", "Ragnhild", "Svanhild",
+      "Thordis", "Valdis", "Vigdis", "Astridur", "Brynhildr",
+      "Eydisara", "Gudmunda", "Hildegund", "Sigriður", "Torunn",
+    ],
+    neutral: [
+      "Ari", "Asa", "Blake", "Eden", "Eir",
+      "Hale", "Jory", "Kiran", "Lenn", "Nico",
+      "Orin", "Rory", "Sage", "Skye", "Torr",
+      "Valen", "Wren", "Yael", "Rune", "Storm",
+    ]
+  },
+  surnames: {
+    occupational: [
+      "Blacksmith", "Ironmonger", "Shipwright", "Stonecarver", "Woodwright",
+      "Bonecarver", "Furrier", "Herder", "Huntsman", "Trapper",
+      "Axemaker", "Bowyer", "Farrier", "Fletcher", "Forger",
+      "Miner", "Ranger", "Sawyer", "Tanner", "Weaver",
+      "Brewer", "Fisher", "Scribe", "Skald", "Trader",
+      "Warden", "Wright", "Carver", "Mason", "Cooper",
+    ],
+    patronymic: [
+      "Arneson", "Bjornson", "Dagson", "Eirikson", "Fenricson",
+      "Gunnarson", "Hakonson", "Halvorson", "Ivarson", "Jorundson",
+      "Kjellson", "Leifson", "Magnarson", "Njalsson", "Oskarson",
+      "Ragnarson", "Rurikson", "Sigurdson", "Svenson", "Thorinsson",
+      "Ulfricson", "Viggoson", "Yorikson", "Arvidson", "Stensson",
+      "Grimsson", "Torsson", "Ulfsson", "Vidarson", "Wulfson",
+    ],
+    toponymic: [
+      "Blackpeak", "Coldhaven", "Deepfjord", "Frostborn", "Frostfell",
+      "Frosthall", "Icecrag", "Icefall", "Icewatch", "Ironpeak",
+      "Ravenhelm", "Rimewood", "Snowborn", "Snowridge", "Stoneborn",
+      "Stonecairn", "Stormborn", "Stormhelm", "Winterborn", "Winterfell",
+      "Wolfcrag", "Wolfden", "Ashenvale", "Coldmere", "Deepwood",
+      "Firthborn", "Fjordborn", "Grimforge", "Ironforge", "Longshore",
+      "Northmarch", "Ravenscar", "Starkfell", "Thornvale", "Wyrmwood",
+    ],
+    ornamental: [
+      "Battleborn", "Bearclaw", "Blackblade", "Bloodaxe", "Dragonheart",
+      "Frostblade", "Icefang", "Ironaxe", "Ironfist", "Ironheart",
+      "Oathkeeper", "Ravenshield", "Shieldbreaker", "Skullcrusher", "Stormaxe",
+      "Stormbringer", "Stormhand", "Strongbow", "Thunderfist", "Warbringer",
+      "Winterblade", "Wolfbane", "Wolfheart", "Wyrmsbane", "Wyvernslayer",
+    ]
+  }
+} as const;
